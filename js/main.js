6f8e1c7953c7a56e45f912bcf5d6b820215ada4c
Aishwarya Mallampati <aishwarya.mallampati@gmail.com>
2020-06-27 01:40:20 -0400

    Base Working Model

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    <!-- Sidebar Toggle Button -->
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    
+}); 
